70d11d13eb2adfb102e08d0a238ad1f82080d316
Mehrdad Najafalizadeh <najafalizadehmehrdad@gmail.com>
2024-07-17 15:03:43 +0430

    production mode swagger

--- Accounting/Program.cs
+++ Accounting/Program.cs
@@ -93,7 +93,7 @@ options => options.UseNodaTime()));
         WebApplication app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
         {
             app.UseSwagger();
             app.UseSwaggerUI();
