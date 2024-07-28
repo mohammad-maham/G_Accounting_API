b065b2e3d25aa396d87e6d6017eee717db41d616
mehrdadnjf <mehrdadnjfs@gmail.com>
2024-07-28 15:43:47 +0330

    --

---
+++ Accounting/Controllers/AttributesController.cs
@@ -0,0 +1,19 @@
+﻿using Accounting.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Accounting.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AttributesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("[action]")]
+        public void GetAuthorize([FromBody] AuthorizationFilterContext context)
+        {
+            AuthorizeAttribute authorizeAttribute = new();
+            authorizeAttribute.OnAuthorization(context);
+        }
+    }
+}
